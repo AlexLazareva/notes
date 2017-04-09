916fd2799b0799fe9e239beccb33b1ef4b4d738c
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-04-09 20:34:43 +0300

    fixed main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
-var NotesApp = require('./components/NotesApp.jsx');
+var NotesApp = require('./components/NotesApp/NotesApp.jsx');
 
 ReactDOM.render(
     <NotesApp />,
